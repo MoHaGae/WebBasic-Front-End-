503ac55ff0231ed4eabcc38cf71779e122760923
sinwoo1225 <48782832+sinwoo1225@users.noreply.github.com>
2020-01-30 13:03:13 +0900

    upload fucntion that aside is fixed
    main컨텐츠 내의 aside(user프로필, 스토리, 추천...) 부분을 사용자가 스크롤을 내린 상태에서 브라우저 사이즈를 조정할때에 대한 처리 및 position: fixed를 적용
    => 현재 잘 고정되어있음 😎

--- shinwoo/shinwoostagram/js/index.js
+++ shinwoo/shinwoostagram/js/index.js
@@ -3,7 +3,8 @@ const nav = document.querySelector("#nav");
 const nav__largecontainer = document.querySelector(".nav__largecontainer");
 const logo__borderline = document.querySelector(".logo__borderline");
 const logo__image = document.querySelector(".logo__image");
-
+const main__aside = document.querySelector(".main__aside");
+const main__article = document.querySelector(".main__article");
 /*
     &&&&&&   
     &&&&&&                                   
@@ -31,6 +32,7 @@ const isScrollTop = () => {
 }
 
 // 상단 네비게이션을 커다란(Lg: large)네비바로 적용하는 함수
+// main_section 내의 aside의 position을 fixed로 적용해제 (cf: aside-fixed 클래스)
 const showNavLg = () => {
     nav.classList.remove("nav-sm");
     nav.classList.add("nav-lg");
@@ -38,9 +40,12 @@ const showNavLg = () => {
     nav__largecontainer.classList.add("nav__largecontainer-lg");
     logo__borderline.classList.remove("opacity-zero");
     logo__image.classList.remove("opacity-zero");
+    main__aside.classList.remove("aside-fixed"); //aside 고정해제
+    main__aside.style = "";
 }
 
 // 상단 네비게이션을 작은(Sm: small)네비바로 적용
+// main_section 내의 aside의 position을 fixed로 적용 (cf: aside-fixed 클래스)
 const showNavSm = () => {
     nav.classList.add("nav-sm");
     nav.classList.remove("nav-lg");
@@ -48,19 +53,33 @@ const showNavSm = () => {
     nav__largecontainer.classList.add("nav__largecontainer-sm");
     logo__borderline.classList.add("opacity-zero");
     logo__image.classList.add("opacity-zero");
+    main__aside.classList.add("aside-fixed"); // aside 고정(position:fixed 적용)
+    applyAsideFixedLeft();
 }
 
 // 스크롤 이벤트를 처리하는 함수
-const handleScroll = (e) => {
+const handleScroll = e => {
     if (!isScrollTop())
         showNavSm();
     else
         showNavLg();
 };
 
+const applyAsideFixedLeft = () => {
+    const article_posX = main__article.getBoundingClientRect().x;
+    main__aside.style = `left:${article_posX + 28 + 614}px`;
+};
+
+// 브라우저 화면크기 재조정시 이벤트 처리
+const handleResize = e => {
+    if (!isScrollTop())
+        applyAsideFixedLeft();
+};
+
 //자바스크립트 초기 실행 함수
 const init = () => {
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
 };
 
 //초기 실행 함수 호출
